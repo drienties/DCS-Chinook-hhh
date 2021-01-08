bcbfe1bd40a04166c0ef7149b60362c942180435
drienties <drienties@gmail.com>
2021-01-08 11:55:31 +0100

    localized vars

--- chinook.lua
+++ chinook.lua
@@ -1,149 +1,149 @@
-SupportHandler = EVENTHANDLER:New()
+hhh-SupportHandler = EVENTHANDLER:New()
 --test
-TimeOnGround = 60
+hhh-TimeOnGround = 60
 
-InfAlias = 1
+hhh-InfAlias = 1
 
-local MissionSchedule = SCHEDULER:New( nil, 
+local hhh-MissionSchedule = SCHEDULER:New( nil, 
   function()
-    --check chinook status
+    --check hhh-chinook status
 	--MessageAll = MESSAGE:New( "Check",  25):ToAll()
 	for i = 1, 2, 1
 	do
-		local ChinookUnit = "CH_"..ReturnCoalitionName(i)
-		local ChinookInf = GROUP:FindByName( ChinookUnit.." - infantry#001" )
-		local ChinookFlag = GROUP:FindByName( ChinookUnit.." - flag#001" )
+		local hhh-ChinookUnit = "CH_"..hhh-ReturnCoalitionName(i)
+		local hhh-ChinookInf = GROUP:FindByName( hhh-ChinookUnit.." - infantry#001" )
+		local hhh-ChinookFlag = GROUP:FindByName( hhh-ChinookUnit.." - flag#001" )
 		
-		if ChinookInf ~= nil then
-			chinook = ChinookInf
-		elseif ChinookFlag ~= nil then
-			Chinook = ChinookFlag
+		if hhh-ChinookInf ~= nil then
+			hhh-chinook = hhh-ChinookInf
+		elseif hhh-ChinookFlag ~= nil then
+			hhh-chinook = hhh-ChinookFlag
 		end
 				
-		if Chinook ~= nil then
-			if Chinook ~= nil then
-				CheckChinookTasks(i)
+		if hhh-chinook ~= nil then
+			if hhh-chinook ~= nil then
+				hhh-CheckChinookTasks(i)
 			end
 		end
 	end
   end, {}, 1, 10
   )
 
-function CheckChinookTasks(coalition)
-	local ChinookUnit = "CH_"..ReturnCoalitionName(coalition)
-	local ChinookInf = GROUP:FindByName( ChinookUnit.." - infantry#001" )
-	local ChinookFlag = GROUP:FindByName( ChinookUnit.." - flag#001" )
+function hhh-CheckChinookTasks(hhh-coalition)
+	local hhh-ChinookUnit = "CH_"..hhh-ReturnCoalitionName(hhh-coalition)
+	local hhh-ChinookInf = GROUP:FindByName( hhh-ChinookUnit.." - infantry#001" )
+	local hhh-ChinookFlag = GROUP:FindByName( hhh-ChinookUnit.." - flag#001" )
 	
-	if ChinookInf ~= nil then
+	if hhh-ChinookInf ~= nil then
 		-- inf tasks
-		local StartZone = "SZ_"..ReturnCoalitionName(coalition)
-		local CH_Zone = ZONE:FindByName(StartZone)
-		if (ChinookInf:IsNotInZone(CH_Zone) == true) and (ChinookInf:InAir() == false) then
-			--MessageAll = MESSAGE:New( "Chinook Landed at LZ",  25):ToAll()
-			SpawnInfantry(coalition, ChinookInf)
-		elseif ChinookInf:GetFuel() < 0.99 and ChinookInf:InAir() ~= true then
-			ChinookInf:ClearTasks()
-			ChinookInf:Destroy()
-			--SpawnChinookStatic(Side)
+		local hhh-StartZone = "SZ_"..hhh-ReturnCoalitionName(hhh-coalition)
+		local hhh-CH_Zone = ZONE:FindByName(hhh-StartZone)
+		if (hhh-ChinookInf:IsNotInZone(hhh-CH_Zone) == true) and (hhh-ChinookInf:InAir() == false) then
+			--MessageAll = MESSAGE:New( "hhh-chinook Landed at LZ",  25):ToAll()
+			hhh-SpawnInfantry(hhh-coalition, hhh-ChinookInf)
+		elseif hhh-ChinookInf:GetFuel() < 0.99 and hhh-ChinookInf:InAir() ~= true then
+			hhh-ChinookInf:ClearTasks()
+			hhh-ChinookInf:Destroy()
+			--hhh-SpawnChinookStatic(Side)
 		end
 		
 	end
 	
-	if ChinookFlag ~= nil then
+	if hhh-ChinookFlag ~= nil then
 		-- flag tasks
-		local StartZone = "SZ_"..ReturnCoalitionName(coalition)
-		local CH_Zone = ZONE:FindByName(StartZone)
-		if (ChinookFlag:IsNotInZone(CH_Zone) == true) and (ChinookFlag:InAir() == false) then
-			MessageAll = MESSAGE:New( "Chinook Landed at LZ",  25):ToAll()
-			if coalition == 1 then
+		local hhh-StartZone = "SZ_"..hhh-ReturnCoalitionName(hhh-coalition)
+		local hhh-CH_Zone = ZONE:FindByName(hhh-StartZone)
+		if (hhh-ChinookFlag:IsNotInZone(hhh-CH_Zone) == true) and (hhh-ChinookFlag:InAir() == false) then
+			MessageAll = MESSAGE:New( "hhh-chinook Landed at LZ",  25):ToAll()
+			if hhh-coalition == 1 then
 				trigger.action.setUserFlag('100', 1)
-			elseif coalition == 2 then
+			elseif hhh-coalition == 2 then
 				trigger.action.setUserFlag('200', 1)
 			end
-		elseif ChinookFlag:GetFuel() < 0.99 and ChinookFlag:InAir() ~= true then
-			ChinookFlag:ClearTasks()
-			ChinookFlag:Destroy()
-			--SpawnChinookStatic(Side)
+		elseif hhh-ChinookFlag:GetFuel() < 0.99 and hhh-ChinookFlag:InAir() ~= true then
+			hhh-ChinookFlag:ClearTasks()
+			hhh-ChinookFlag:Destroy()
+			--hhh-SpawnChinookStatic(Side)
 		end
 	end
 	
 end
 
-function ReturnCoalitionName(coalition)
- if coalition == 1 then
+function hhh-ReturnCoalitionName(hhh-coalition)
+ if hhh-coalition == 1 then
 	return "Red"
- elseif coalition == 2 then
+ elseif hhh-coalition == 2 then
 	return "Blue"
  end
 end
 
-function SpawnInfantry(coalition, location)
-	--MessageAll = MESSAGE:New( ReturnCoalitionName(coalition).." Chinook Unloading troops at LZ",  25):ToAll()
-	InfUnit = "Inf_"..ReturnCoalitionName(coalition)
-    Spawn_Inf = SPAWN:NewWithAlias(InfUnit, InfAlias)
+function hhh-SpawnInfantry(hhh-coalition, hhh-location)
+	--MessageAll = MESSAGE:New( hhh-ReturnCoalitionName(hhh-coalition).." hhh-chinook Unloading troops at LZ",  25):ToAll()
+	hhh-InfUnit = "Inf_"..hhh-ReturnCoalitionName(hhh-coalition)
+    hhh-Spawn_Inf = SPAWN:NewWithAlias(hhh-InfUnit, hhh-InfAlias)
  
-    local StartLoc = location:GetVec3()
-    Spawn_Inf:SpawnFromVec3( StartLoc )
-	InfAlias = InfAlias + 1
-	location:PopCurrentTask()
+    local hhh-StartLoc = hhh-location:GetVec3()
+    hhh-Spawn_Inf:SpawnFromVec3( hhh-StartLoc )
+	hhh-InfAlias = hhh-InfAlias + 1
+	hhh-location:PopCurrentTask()
 end
 
-function SpawnChinookStatic(coalition)
-	CoalitionVars(coalition)
-	local UnitALias = ChinookAlias
-	local Spawn_Static = SPAWNSTATIC:NewFromType("CH-47D", "Helicopters", country.id.USA )
-	local CH_Zone = ZONE:FindByName(StartZone)
-	Spawn_Static:SpawnFromZone(CH_Zone)
+function hhh-SpawnChinookStatic(hhh-coalition)
+	CoalitionVars(hhh-coalition)
+	local hhh-UnitALias = ChinookAlias
+	local hhh-Spawn_Static = SPAWNSTATIC:NewFromType("CH-47D", "Helicopters", country.id.USA )
+	local hhh-CH_Zone = ZONE:FindByName(hhh-StartZone)
+	hhh-Spawn_Static:SpawnFromZone(hhh-CH_Zone)
 end
 
-function SpawnChinook(coord, coalition, task)
+function hhh-SpawnChinook(hhh-coord, hhh-coalition, hhh-task)
  
-	local DestLoc = coord 
-	local ChinookUnit = "CH_"..ReturnCoalitionName(coalition)
-	local StartZone = "SZ_"..ReturnCoalitionName(coalition)
-	local ChinookAlias = ChinookUnit.." - ".. task
+	local hhh-DestLoc = hhh-coord 
+	local hhh-ChinookUnit = "CH_"..hhh-ReturnCoalitionName(hhh-coalition)
+	local hhh-StartZone = "SZ_"..hhh-ReturnCoalitionName(hhh-coalition)
+	local ChinookAlias = hhh-ChinookUnit.." - ".. hhh-task
 	
-	local Spawn_Plane = SPAWN:NewWithAlias( ChinookUnit, ChinookAlias ):InitLimit( 1, 1 )
-	local CH_Zone = ZONE:FindByName(StartZone)
-	Spawn_Plane:SpawnInZone(CH_Zone)
+	local Spawn_Plane = SPAWN:NewWithAlias( hhh-ChinookUnit, ChinookAlias ):InitLimit( 1, 1 )
+	local hhh-CH_Zone = ZONE:FindByName(hhh-StartZone)
+	Spawn_Plane:SpawnInZone(hhh-CH_Zone)
 
-	Chinook = GROUP:FindByName( ChinookAlias.."#001" )
+	hhh-chinook = GROUP:FindByName( ChinookAlias.."#001" )
 	 
-	local LandHeloLZ = Chinook:TaskLandAtVec2(DestLoc:GetVec2(),TimeOnGround) --60
-	local LandHeloRTB = Chinook:TaskLandAtVec2(CH_Zone:GetVec2(),500) --500
-	Chinook:OptionROTNoReaction()
-	Chinook:OptionROEWeaponFree()
-	Chinook:PushTask(LandHeloRTB, 1)
-	Chinook:PushTask(LandHeloLZ, 1)
+	local LandHeloLZ = hhh-chinook:TaskLandAtVec2(hhh-DestLoc:GetVec2(),hhh-TimeOnGround) --60
+	local LandHeloRTB = hhh-chinook:TaskLandAtVec2(hhh-CH_Zone:GetVec2(),500) --500
+	hhh-chinook:OptionROTNoReaction()
+	hhh-chinook:OptionROEWeaponFree()
+	hhh-chinook:PushTask(LandHeloRTB, 1)
+	hhh-chinook:PushTask(LandHeloLZ, 1)
 end
 
 
-function markRemoved(Event)
-    if Event.text~=nil then 
-        local text = Event.text:lower()
-        local vec3 = {z=Event.pos.z, x=Event.pos.x}
-		local coalition = Event.coalition
-        local coord = COORDINATE:NewFromVec3(vec3)
-			if Event.text:lower():find("a") then
-				local task = "infantry"
-				SpawnChinook(coord, coalition, task)
+function hhh-markRemoved(hhh-Event)
+    if hhh-Event.hhh-text~=nil then 
+        local hhh-text = hhh-Event.hhh-text:lower()
+        local hhh-vec3 = {z=hhh-Event.pos.z, x=hhh-Event.pos.x}
+		local hhh-coalition = hhh-Event.hhh-coalition
+        local hhh-coord = COORDINATE:NewFromVec3(hhh-vec3)
+			if hhh-Event.hhh-text:lower():find("a") then
+				local hhh-task = "infantry"
+				hhh-SpawnChinook(hhh-coord, hhh-coalition, hhh-task)
 			end
-			if Event.text:lower():find("b") then
-				local task = "flag"
-				SpawnChinook(coord, coalition, task)
+			if hhh-Event.hhh-text:lower():find("b") then
+				local hhh-task = "flag"
+				hhh-SpawnChinook(hhh-coord, hhh-coalition, hhh-task)
 			end
     end
 end
 
-function SupportHandler:onEvent(Event)
-    if Event.id == world.event.S_EVENT_MARK_ADDED then
-        -- env.info(string.format("BTI: Support got event ADDED id %s idx %s coalition %s group %s text %s", Event.id, Event.idx, Event.coalition, Event.groupID, Event.text))
-    elseif Event.id == world.event.S_EVENT_MARK_CHANGE then
-        -- env.info(string.format("BTI: Support got event CHANGE id %s idx %s coalition %s group %s text %s", Event.id, Event.idx, Event.coalition, Event.groupID, Event.text))
-    elseif Event.id == world.event.S_EVENT_MARK_REMOVED then
-        -- env.info(string.format("BTI: Support got event REMOVED id %s idx %s coalition %s group %s text %s", Event.id, Event.idx, Event.coalition, Event.groupID, Event.text))
-        markRemoved(Event)
+function hhh-SupportHandler:onEvent(hhh-Event)
+    if hhh-Event.id == world.hhh-Event.S_EVENT_MARK_ADDED then
+        -- env.info(string.format("BTI: Support got hhh-Event ADDED id %s idx %s hhh-coalition %s group %s hhh-text %s", hhh-Event.id, hhh-Event.idx, hhh-Event.hhh-coalition, hhh-Event.groupID, hhh-Event.hhh-text))
+    elseif hhh-Event.id == world.hhh-Event.S_EVENT_MARK_CHANGE then
+        -- env.info(string.format("BTI: Support got hhh-Event CHANGE id %s idx %s hhh-coalition %s group %s hhh-text %s", hhh-Event.id, hhh-Event.idx, hhh-Event.hhh-coalition, hhh-Event.groupID, hhh-Event.hhh-text))
+    elseif hhh-Event.id == world.hhh-Event.S_EVENT_MARK_REMOVED then
+        -- env.info(string.format("BTI: Support got hhh-Event REMOVED id %s idx %s hhh-coalition %s group %s hhh-text %s", hhh-Event.id, hhh-Event.idx, hhh-Event.hhh-coalition, hhh-Event.groupID, hhh-Event.hhh-text))
+        hhh-markRemoved(hhh-Event)
     end
 end
 
-world.addEventHandler(SupportHandler)
+world.addEventHandler(hhh-SupportHandler)
